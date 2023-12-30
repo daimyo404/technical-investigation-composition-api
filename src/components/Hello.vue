abe7a80294497b5c8053caeeca24793ef13df1c6
hiroaki-saito <hiro021312@gmail.com>
2023-12-30 22:45:18 +0900

    Initial commit.

---
+++ src/components/Hello.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, reactive } from "vue";
+const count = ref(0);
+const state = reactive({
+  count: 1,
+});
+
+const addRefCount = () => {
+  count.value++;
+  state.count++;
+};
+
+const props = defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <h2>コンポーネント</h2>
+  <p>count:{{ count }}</p>
+  <p>state:{{ state.count }}</p>
+  <div>
+    <button @click="addRefCount">count++</button>
+  </div>
+  <p>{{ props.msg }}</p>
+</template>
